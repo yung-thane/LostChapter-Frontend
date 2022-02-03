faa4d1beda0b55713b93ea5735b7149ca5319cfa
wilderchris <94183821+wilderchris@users.noreply.github.com>
2022-02-02 20:13:28 -0500

    services updated to match the back end end points
    only tested sign up and login since no data in my local DB

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -8,14 +8,16 @@ export class LoginService {
   constructor(private http: HttpClient) {}
 
   checkLoginStatus() {
-    return this.http.get('http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/loginstatus', {
+    return this.http.get(`http://localhost:8081/loginstatus`,{
+      //'http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/loginstatus', {
       observe: 'response',
       withCredentials: true,
     });
   }
 
   login(username: string, password: string){
-    return this.http.post('http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/login', {
+    return this.http.post(`http://localhost:8081/login`,{
+      //'http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/login', {
       "username": username,
       "password": password
     }, {
@@ -25,8 +27,8 @@ export class LoginService {
   }
 
   logout() {
-    return this.http.post(
-      'http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/logout',
+    return this.http.post(`http://localhost:8081/logout`,
+      //'http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/logout',
       {},
       {
         observe: 'response',
@@ -46,8 +48,8 @@ export class LoginService {
     address: string,
     role: string
   ) {
-    return this.http.put(
-      `http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/user`,
+    return this.http.put(`http://localhost:8081/user`,
+     // `http://ec2-54-84-57-117.compute-1.amazonaws.com:8081/user`,
       {
         username: username,
         password: password,
