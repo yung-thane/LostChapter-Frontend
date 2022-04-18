3747101cdda30a4f83e427ae071fde9158ec88b6
DaisyRdrguez <daisy.rod19@gmail.com>
2022-04-18 15:54:47 -0500

    initial commit - wishlist by daisy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,6 +43,7 @@ import { AdminFeatureComponent } from './admin-feature/admin-feature.component';
 import { UpdateBookComponent } from './update-book/update-book.component';
 import { FeaturedProductsComponent } from './featured-products/featured-products.component';
 import { NgxSlickJsModule } from 'ngx-slickjs';
+import { WishlistComponent } from './wishlist/wishlist/wishlist.component';
 
 @NgModule({
   declarations: [
@@ -61,7 +62,8 @@ import { NgxSlickJsModule } from 'ngx-slickjs';
     SalesProductComponent,
     AdminFeatureComponent,
     UpdateBookComponent,
-    FeaturedProductsComponent
+    FeaturedProductsComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule,
